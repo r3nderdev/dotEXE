0d38524250a11ae3ecc5202508374a3e8abf8afa
Mixu <mikohurme@gmail.com>
2024-08-18 12:45:00 +0300

    Simplified DoorOpen Script
    Now it is fully functional with settings and tooltips

--- Assets/Scripts/DoorOpen.cs
+++ Assets/Scripts/DoorOpen.cs
@@ -1,31 +1,45 @@
 using System.Collections;
-using UnityEditor;
 using UnityEngine;
 
 public class DoorOpen : MonoBehaviour
 {
-    [Header("Kill Trigger")]
-    [HideInInspector] public bool killTrigger;
-    [HideInInspector] public int killsToOpen;
+    [Header("What Opens The Door")]
+    [Tooltip("Enable this to open the door based on kill count.")]
+    [SerializeField] private bool killTrigger;
 
-    [Header("Dialogue Trigger")]
-    [HideInInspector] public bool dialogueTrigger;
-    [HideInInspector] public int indexToOpen;
+    [Tooltip("Number of kills required to open the door.")]
+    [Range(0, 25)]
+    [SerializeField] private int killsToOpen;
 
-    [Header("Collider Trigger")]
-    [HideInInspector] public bool colliderTrigger;
+    [Space(10)]
+    [Tooltip("Enable this to open the door based on dialogue.")]
+    [SerializeField] private bool dialogueTrigger;
 
-    [Header("Door Opening")]
-    [HideInInspector] public float openPosition = 7f;
-    [HideInInspector] public float openSpeed = 2f;
+    [Tooltip("Index of the dialogue that triggers the door to open.")]
+    [Range(0,20)]
+    [SerializeField] private int indexToOpen;
 
-    [Header("Dialogue")]
-    [HideInInspector] public Dialogue dialogue;
-    [HideInInspector] public string[] strings;
+    [Space(10)]
+    [Tooltip("Enable to open the door when the player enters the collider.")]
+    [SerializeField] private bool colliderTrigger;
+
+    [Header("Door Settings")]
+    [Tooltip("Y position to which the door will open.")]
+    [SerializeField] private float openPosition = 7f;
+
+    [Tooltip("Speed at which the door opens. 2 is the default value.")]
+    [Range(0, 15)]
+    [SerializeField] private float openSpeed = 2f;
+
+    [Header("Dialogue On Open")]
+    [Tooltip("Reference to the dialogue script.")]
+    [SerializeField] private Dialogue dialogueScript;
+
+    [Tooltip("Dialogue to be showed when the door opens. Leave empty for no dialogue.")]
+    [SerializeField] private string[] strings;
 
     private bool isOpening = false;
     private bool hasEntered = false;
-
 
     void Start()
     {
@@ -38,28 +52,27 @@ public class DoorOpen : MonoBehaviour
     {
         yield return new WaitUntil(() => PlayerHealth.killCount >= killsToOpen);
         isOpening = true;
-        dialogue.StartDialogue(strings);
+        dialogueScript.StartDialogue(strings);
     }
 
     IEnumerator OpenDoorByDialogue()
     {
-        dialogue.StartDialogue(strings);
-        yield return new WaitUntil(() => dialogue.index == indexToOpen);
+        dialogueScript.StartDialogue(strings);
+        yield return new WaitUntil(() => dialogueScript.index == indexToOpen);
         isOpening = true;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player") && hasEntered == false) hasEntered = true;
+        if (other.CompareTag("Player") && !hasEntered) hasEntered = true;
     }
 
     IEnumerator OpenDoorByCollider()
     {
-        yield return new WaitUntil(() => hasEntered == true);
-        dialogue.StartDialogue(strings);
+        yield return new WaitUntil(() => hasEntered);
+        dialogueScript.StartDialogue(strings);
         isOpening = true;
     }
-
 
     void Update()
     {
@@ -67,52 +80,5 @@ public class DoorOpen : MonoBehaviour
         {
             transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, openPosition, transform.position.z), openSpeed * Time.deltaTime);
         }
-    }
-}
-
-
-[CustomEditor(typeof(DoorOpen))]
-public class Door_Editor : Editor
-{
-    private SerializedProperty stringsProperty;
-
-    public void OnEnable()
-    {
-        stringsProperty = serializedObject.FindProperty("strings");
-    }
-
-    public override void OnInspectorGUI()
-    {
-        serializedObject.Update();
-
-        var script = (DoorOpen)target;
-
-
-        EditorGUILayout.LabelField("What Opens The Door", EditorStyles.boldLabel); // Trigger Settings
-
-        script.killTrigger = EditorGUILayout.Toggle("Killcount", script.killTrigger); // Kills as trigger
-        if (script.killTrigger)
-        {
-            script.killsToOpen = EditorGUILayout.IntField("Kills To Open: ", script.killsToOpen);
-        }
-        script.dialogueTrigger = EditorGUILayout.Toggle("Dialogue Index", script.dialogueTrigger); // Dialogue as trigger
-        if (script.dialogueTrigger)
-        {
-            script.indexToOpen = EditorGUILayout.IntField("Index To Open on:", script.indexToOpen);
-        }
-        script.colliderTrigger = EditorGUILayout.Toggle("Trigger Collider", script.colliderTrigger); // Collider as trigger
-
-        EditorGUILayout.Space();
-
-        EditorGUILayout.LabelField("Door Settings", EditorStyles.boldLabel); // Door Settings
-        script.openPosition = EditorGUILayout.FloatField("Open Y Position", script.openPosition);
-        script.openSpeed = EditorGUILayout.FloatField("Door Opening Speed", script.openSpeed);
-
-        EditorGUILayout.Space();
-
-        EditorGUILayout.LabelField("Dialogue", EditorStyles.boldLabel);// Dialogue Settings
-        script.dialogue = EditorGUILayout.ObjectField("Dialogue Script:", script.dialogue, typeof(Dialogue), true) as Dialogue; // Dialogue script reference
-        EditorGUILayout.PropertyField(stringsProperty, new GUIContent("Dialogue on Open"), true); // String array
-        serializedObject.ApplyModifiedProperties();
     }
 }
