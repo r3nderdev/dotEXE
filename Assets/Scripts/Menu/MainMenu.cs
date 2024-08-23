b2cf680ea91a5ed1f5ee77ae92d3b00544a5659c
Mixu <mikohurme@gmail.com>
2024-08-24 00:41:04 +0300

    Added Version Text to Main Menu
    Also removed sensitivity slider from Main Menu Settings

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -9,10 +9,12 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private Animator cameraAnimator;
     [SerializeField] private Button[] menuButtons;
     private TMP_Text[] buttonTexts;
+    [SerializeField] private TMP_Text versionText;
 
     [SerializeField] private GameObject computerUI;
 
-    [Header("Delay")]
+    [Header("Settings")]
+    [SerializeField] private float fadeTime = 0.7f;
     [SerializeField] private float animationDelay = 4f;
 
     private void Start()
@@ -56,16 +58,18 @@ public class MainMenu : MonoBehaviour
         // Fade UI
         float elapsedTime = 0f;
 
-        while (elapsedTime < 0.7f)
+        while (elapsedTime < fadeTime)
         {
             elapsedTime += Time.deltaTime;
 
             foreach (TMP_Text text in buttonTexts)
             {
                 Color textColor = text.color;
-                textColor.a = Mathf.Lerp(1, 0, elapsedTime / 0.7f);
+                textColor.a = Mathf.Lerp(1, 0, elapsedTime / fadeTime);
                 text.color = textColor;
             }
+
+            versionText.alpha = Mathf.Lerp(1, 0, elapsedTime / fadeTime);
             yield return null;
         }
 
